725d30d9557205acf6c239978e8324a71b0e1b56
MasterHans <suvorov.ag.75@gmail.com>
2016-05-23 18:01:21 +0300

    Сделал ещё 2 варианта ЧПУ

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
 require_once __DIR__ . '/autoload.php';
 
-$ctrl = isset($_GET['ctrl']) ? $_GET['ctrl'] : 'News';
-$act = isset($_GET['act']) ? $_GET['act'] : 'All';
+$path = parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);
+$pathParts = explode('/',$path);
+
+$ctrl = !empty($pathParts[1]) ? ucfirst($pathParts[1]) : 'News';
+$act = !empty($pathParts[2]) ? ucfirst($pathParts[2]) : 'All';
 
 $controllerClassName = $ctrl . 'Controller';
 
